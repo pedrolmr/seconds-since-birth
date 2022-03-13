922e7e2b966ea70dc558be38a7615cde27f100dc
pedrolmr <pedroluismontesinosrivera@gmail.com>
2022-03-12 20:39:14 -0500

    #

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const calcSeconds = (birthDate) => {
-    let currMonth = new Date().getMonth() + 1;
-    let currYear = new Date().getFullYear();
-    let secsInMonth = 60 * 60 * 24 * 30;
-    let secsInYear = secsInMonth * 12;
+  let currMonth = new Date().getMonth() + 1;
+  let currYear = new Date().getFullYear();
+  let secsInMonth = 60 * 60 * 24 * 30;
+  let secsInYear = secsInMonth * 12;
 
-    let splitBirth = birthDate.split(" ");
-    let birthMonth = Number(splitBirth[0]);
-    let birthYear = Number(splitBirth[1]);
+  let splitBirth = birthDate.split(" ");
+  let birthMonth = Number(splitBirth[0]);
+  let birthYear = Number(splitBirth[1]);
 
-    let secsInBetween = secsInYear * (currYear - birthYear);
-    let secsInBirthYear = secsInMonth * (12 - birthMonth);
-    let secsInCurrYear = secsInMonth * currMonth;
+  let secsInBetween = secsInYear * (currYear - birthYear);
+  let secsInBirthYear = secsInMonth * (12 - birthMonth);
+  let secsInCurrYear = secsInMonth * currMonth;
 
-    let secsSinceBorn = secsInBetween + secsInBirthYear + secsInCurrYear;
+  let secsSinceBorn = secsInBetween + secsInBirthYear + secsInCurrYear;
 
-    return `Seconds since you were born: ${secsSinceBorn}`;
-  }
-  console.log(calcSeconds("05 1998"));
+  return `Seconds since you were born: ${secsSinceBorn}`;
+};
+console.log(calcSeconds("05 1998"));
+
+//
